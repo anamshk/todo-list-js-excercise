e4be90ba765749453aa9b57a2e3a37f2fe150400
anamshk <89604126+anamshk@users.noreply.github.com>
2021-11-06 15:19:49 -0400

    commit to master

---
+++ index.js
@@ -0,0 +1,31 @@
+// Arrays to keep track of each task's state
+// Create a new task by adding to the arrays
+// A new task will be created as incomplete
+const newTask = function(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+};
+
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box"); // task 0
+const task2 = newTask("Do Laundry", "😨"); // task 1
+const tasks = [task1, task2];
+
+console.log(tasks);
+task1.logState(0); // Clean Cat Litter has not been completed
+task1.markCompleted(0);
+task1.logState(0); // Clean Cat Litter has been completed.
